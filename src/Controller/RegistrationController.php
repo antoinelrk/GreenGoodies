60da36d62bca44facc0101f16262065a68bffceb
Antoine LRK <55787169+antoinelrk@users.noreply.github.com>
2025-09-09 07:19:21 +0200

    Make EmailVerifier property readonly in RegistrationController constructor

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -17,7 +17,7 @@ use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
 class RegistrationController extends AbstractController
 {
-    public function __construct(private EmailVerifier $emailVerifier)
+    public function __construct(private readonly EmailVerifier $emailVerifier)
     {
     }
 
