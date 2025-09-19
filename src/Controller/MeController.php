05494654f1cf31ac12a2bdd2ef4022832e0281d8
Antoine LRK <55787169+antoinelrk@users.noreply.github.com>
2025-09-19 08:23:41 +0200

    Implement user account deletion functionality in MeController

--- src/Controller/MeController.php
+++ src/Controller/MeController.php
@@ -3,9 +3,15 @@
 namespace App\Controller;
 
 
+use Doctrine\DBAL\Exception;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 final class MeController extends AbstractController
 {
@@ -15,9 +21,46 @@ final class MeController extends AbstractController
         return $this->render('pages/me.html.twig');
     }
 
-    public function delete(): Response
+    /**
+     * Delete user account.
+     *
+     * @param EntityManagerInterface $entityManager
+     * @param TokenStorageInterface $tokenStorage
+     * @param RequestStack $requestStack
+     * @return Response
+     * @throws Exception
+     */
+    #[Route('/account/delete', name: 'app_remove_account', methods: ['POST'])]
+    public function delete(
+        EntityManagerInterface $entityManager,
+        TokenStorageInterface $tokenStorage,
+        RequestStack $requestStack,
+    ): Response
     {
-        $this->addFlash('success', 'Your account has been deleted.');
+        $user = $this->getUser();
+        if (!$user) {
+            $this->addFlash('error', 'Aucun utilisateur connecté.');
+            return $this->redirectToRoute('app_login');
+        }
+
+        $conn = $entityManager->getConnection();
+        $conn->beginTransaction();
+
+        try {
+            $entityManager->remove($user);
+            $entityManager->flush();
+            $conn->commit();
+        } catch (\Throwable $e) {
+            $conn->rollBack();
+            $this->addFlash('error', 'La suppression du compte a échoué. ('.$e->getMessage().')');
+            return $this->redirectToRoute('app_home');
+        }
+
+        $tokenStorage->setToken(null);
+        $session = $requestStack->getSession();
+        $session->invalidate();
+
+        $this->addFlash('success', 'Votre compte a bien été supprimé.');
         return $this->redirectToRoute('app_home');
     }
 }
