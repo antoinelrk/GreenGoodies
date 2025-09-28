cf8341569d00d909e2ace9a155e3f489b6442687
Antoine LRK <55787169+antoinelrk@users.noreply.github.com>
2025-09-28 08:39:38 +0200

    Add method to retrieve a cart by customer ID with associated cart items

--- src/Repository/CartRepository.php
+++ src/Repository/CartRepository.php
@@ -16,28 +16,14 @@ class CartRepository extends ServiceEntityRepository
         parent::__construct($registry, Cart::class);
     }
 
-//    /**
-//     * @return Cart[] Returns an array of Cart objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('c.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Cart
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    public function get(int $id): Cart
+    {
+        return $this->createQueryBuilder('c')
+            ->leftJoin('c.cartItems', 'ci')->addSelect('ci')
+            ->leftJoin('ci.product', 'p')->addSelect('p')
+            ->andWhere('IDENTITY(c.customer) = :uid')   // ⚠️ on compare l'ID de l'association
+            ->setParameter('uid', $id)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
 }
