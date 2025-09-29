eb413451be504ae0c226a7b79f36ef8259b165d9
Antoine LRK <55787169+antoinelrk@users.noreply.github.com>
2025-09-29 06:35:29 +0200

    Add methods to add and update items in the cart

--- src/Repository/CartItemRepository.php
+++ src/Repository/CartItemRepository.php
@@ -2,17 +2,49 @@
 
 namespace App\Repository;
 
+use App\Entity\Cart;
 use App\Entity\CartItem;
+use App\Entity\Product;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @extends ServiceEntityRepository<CartItem>
  */
 class CartItemRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    public function __construct(
+        ManagerRegistry $registry,
+        protected readonly EntityManagerInterface $entityManager
+    )
     {
         parent::__construct($registry, CartItem::class);
+    }
+
+    /**
+     * Add an item to the cart.
+     *
+     * @param Cart    $cart
+     * @param Product $product
+     * @param Request $request
+     *
+     * @return CartItem
+     */
+    public function addItemToCart(Cart $cart, Product $product, Request $request): CartItem
+    {
+        $quantity = $request->request->get('quantity', 1);
+        $subTotal = $product->getPrice() * $quantity;
+
+        $item = new CartItem();
+        $item->setCart($cart);
+        $item->setProduct($product);
+        $item->setQuantity($quantity);
+        $item->setSubTotal($subTotal);
+        $this->entityManager->persist($item);
+        $this->entityManager->flush();
+
+        return $item;
     }
 }
