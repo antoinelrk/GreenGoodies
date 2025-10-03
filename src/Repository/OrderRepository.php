2aa73a5e579cfbd82b03da0ca09521d4d466d301
Antoine LRK <55787169+antoinelrk@users.noreply.github.com>
2025-10-03 08:41:44 +0200

    Add OrderController and OrderRepository for order processing

--- src/Repository/OrderRepository.php
+++ src/Repository/OrderRepository.php
@@ -2,8 +2,10 @@
 
 namespace App\Repository;
 
+use App\Entity\Cart;
 use App\Entity\Order;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -11,7 +13,10 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class OrderRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    public function __construct(
+        ManagerRegistry $registry,
+        protected EntityManagerInterface $entityManager
+    )
     {
         parent::__construct($registry, Order::class);
     }
@@ -24,5 +29,18 @@ class OrderRepository extends ServiceEntityRepository
             ->orderBy('o.created_at', 'DESC')
             ->getQuery()
             ->getResult();
+    }
+
+    public function createOrderFromCart(Cart $cart): Order
+    {
+        $order = new Order();
+        $order->setCustomer($cart->getCustomer());
+        $order->setTotalAmount($cart->getTotalPrice());
+        $order->setCreatedAt(new \DateTimeImmutable());
+
+        $this->entityManager->persist($order);
+        $this->entityManager->flush();
+
+        return $order;
     }
 }
